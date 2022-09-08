3bcdca0ea15e54965e40d81569319b5d775e7e50
William Barrera <65841343+WillB5@users.noreply.github.com>
2022-09-08 15:53:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    val editText = findViewById<EditText>(R.id.cardName)
+    val submitButton = findViewById<Button>(R.id.submitButton)
+
+    fun onClick(){
+
+    }
+}
