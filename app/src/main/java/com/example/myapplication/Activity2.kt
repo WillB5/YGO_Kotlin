5f4b379376939f93df88f8fe5efcb4d0029db504
William Barrera <65841343+WillB5@users.noreply.github.com>
2022-09-10 21:28:39 -0500

    added 2nd activity, currently working on properly transferring string cardName to 2nd activity as it currently causes app to crash.

--- app/src/main/java/com/example/myapplication/Activity2.kt
+++ app/src/main/java/com/example/myapplication/Activity2.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+        val cardName = intent.getStringExtra("cardName")
+        val textView = findViewById<TextView>(R.id.displayName)
+
+
+        textView.setText("hello"+ cardName)
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val editText = findViewById<EditText>(R.id.cardName)
+
         val submitButton = findViewById<Button>(R.id.submitButton)
+
+        submitButton.setOnClickListener{
+            val editText = findViewById<EditText>(R.id.cardName)
+            val intent = Intent(this@MainActivity, Activity2::class.java)
+            intent.putExtra("cardName", editText.toString())
+            startActivity(intent)
+        }
     }
-
-    fun onClick(){
-
-    }
-
 }
