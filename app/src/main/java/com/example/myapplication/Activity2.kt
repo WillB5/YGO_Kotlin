d3a24e098f22fc128bbcb6316003f6485ee91469
William Barrera <65841343+WillB5@users.noreply.github.com>
2022-09-10 20:24:54 -0500

    update

--- app/src/main/java/com/example/myapplication/Activity2.kt
+++ app/src/main/java/com/example/myapplication/Activity2.kt
@@ -3,9 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ResultsScreen : AppCompatActivity() {
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_results_screen)
+        setContentView(R.layout.activity_2)
     }
 }
